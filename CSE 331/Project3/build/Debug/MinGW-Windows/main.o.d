be073735dbb4b45eab5a74e50d5258caee4cd234
RamundHinUnge <bollavar@msu.edu>
2016-03-07 13:23:57 -0500

    Added 331 folder with Project3

---
+++ CSE 331/Project3/build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,8 @@
+build/Debug/MinGW-Windows/main.o: main.cpp mystack.h myminstack.h \
+ myqueue.h
+
+mystack.h:
+
+myminstack.h:
+
+myqueue.h:
---
+++ CSE 331/Project3/myqueue.h
@@ -0,0 +1,37 @@
+#ifndef myqueue_h
+#define myqueue_h
+
+#include "mystack.h"
+using namespace std;
+
+template <typename T>
+class myqueue
+{
+private:
+    
+public:
+    void push(T item)
+    {
+        
+    }
+    
+    void pop()
+    {
+        
+    }
+    
+    T gettop()
+    {
+        
+    }
+    
+    bool empty()
+    {
+        
+    }
+};
+
+
+
+
+# endif /*myqueue_h*/
