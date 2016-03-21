99fa2f960f05867b17a5ab518145fee43c552673
MikeMei <mhmei95@gmail.com>
2016-03-21 16:18:56 -0400

    CSE335_project03 #2

--- CSE335_project03/CSE335_proj03/Group.h
+++ CSE335_project03/CSE335_proj03/Group.h
@@ -14,6 +14,75 @@
 #ifndef GROUP_H
 #define GROUP_H
 
+#include <iostream>
+#include "Employee.h"
+#include "Manager.h"
+using namespace std;
+
+class Group
+{
+protected:
+    vector <Employee*>emp_list;
+    
+public:
+    /* Default constructor, does nothing 
+     */
+    Group() {
+        
+    }
+    /* Constructor for CDatabase
+     */
+    Group(vector<Employee*> list)
+    {
+        emp_list = list;
+    }
+    /* Copy Constructor for CDatabase
+     */
+    Group(const Group &otherGroup) {
+        emp_list = otherGroup.getEmployeeDatabase();
+    }
+    /* operator= for CDatabase
+     */
+    Group operator=(const Group &otherDB) {
+        emp_list = otherDB.getEmployeeDatabase();
+        return *this;
+    }
+    /* Setter method for CDatabase, adds a CEmployee pointer to the vector emp_list
+     */
+    void AddGroupMember(Employee* emp)
+    {
+        emp_list.push_back(emp);
+    }
+    /* Displays all employees in emp_list by calling the DisplayEmployee method in the CEmployee class
+     */
+    void DisplayRecords()
+    {
+        for(Employee* emp:emp_list)
+        {
+            emp->DisplayEmployee();
+        }
+    }
+    /* Getter method, returns the list of CEmployee pointers in CDatabase
+     */
+    vector<Employee*> getEmployeeDatabase() const
+    {
+        return emp_list;
+    }
+    /* Setter method, sets the vector<CEmployee*> equal to another CDatabase*/
+    void setEmployeeDatabase(Group emp)
+    {
+        emp_list = emp.getEmployeeDatabase();
+    }
+    /* Getter method, returns a CEmployee pointer in emp_list using the index i
+     */
+    Employee* getEmployee(int i) const{
+       
+        return emp_list[i];
+    }
+    unsigned int getGroupSize() const{
+        return emp_list.size(); 
+    }
+};
 
 
 #endif /* GROUP_H */
--- CSE335_project03/CSE335_proj03/Manager.h
+++ CSE335_project03/CSE335_proj03/Manager.h
@@ -14,6 +14,67 @@
 #ifndef MANAGER_H
 #define MANAGER_H
 
+#include "Employee.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Manager:public Employee {
+protected:
+    string m_departmentName;
+public:
+    /* Default Constructor for CManager
+     */
+    Manager() {
+        m_firstName = "";
+        m_lastName = "";
+        m_salary = 0;
+        m_hiringYear.tm_year = 1900;
+        m_departmentName = "";
+    }
+    /* Constructor for CManager
+     */
+    Manager(string firstName, string lastName, unsigned int salary, tm hiringYear, string departmentName)
+    {
+        m_firstName = firstName;
+        m_lastName = lastName;
+        m_salary = salary;
+        m_hiringYear= hiringYear;
+        m_departmentName = departmentName;
+    }
+    /* Copy Constructor for CManager, copy by reference
+     */
+    Manager(Manager& man){
+        m_firstName = man.getFirstName();
+        m_lastName = man.getLastName();
+        m_salary = man.getSalary();
+        m_hiringYear = man.getHiringYear();
+        m_departmentName = man.getDepartmentName();
+    }
+    /* operator= Constructor for CManager
+     */
+    Manager& operator=(const Manager& man){
+        m_firstName = man.getFirstName();
+        m_lastName = man.getLastName();
+        m_salary = man.getSalary();
+        m_hiringYear = man.getHiringYear();
+        m_departmentName = man.getDepartmentName();
+        return *this;
+    }
+    /* Getter method for CManager, returns the value of m_departmentName
+     */
+    string getDepartmentName() const
+    {
+        return m_departmentName;
+    }
+    /* Displays a CManager, overrides the version in CEmployee
+     */
+    virtual void DisplayEmployee() const
+    {
+        cout << m_firstName << " " << m_lastName << "; " << m_salary << "; " << m_hiringYear.tm_year << "; " << m_departmentName << endl;
+    }
+};
 
 
 #endif /* MANAGER_H */
